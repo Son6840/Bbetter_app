c06ac2ebdedb3978c933c6c6ec96c7cffd6feb29
son6840 <sangbae6840@gmail.com>
2022-04-06 14:38:31 +0900

    Initial Commit

---
+++ Bbetter_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bbetter_app
+//
+//  Created by 모바일인터넷과 on 2022/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
