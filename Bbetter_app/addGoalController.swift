5d76187c888c0c372df645243b290ec7c6fdad09
son <sangbae6840@gmail.com>
2022-04-20 17:50:26 +0900

    addGoal 화면 제작

---
+++ Bbetter_app/addGoalController.swift
@@ -0,0 +1,20 @@
+//
+//  AddGoalController.swift
+//  Bbetter_app
+//
+//  Created by 모바일인터넷과 on 2022/04/20.
+//
+
+import UIKit
+
+class AddGoalController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
